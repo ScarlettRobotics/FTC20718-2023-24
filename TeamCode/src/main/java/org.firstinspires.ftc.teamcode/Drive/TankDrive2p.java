a74b681e3f846bb19438f8a4bcb3e412d479a446
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2023-10-30 16:22:26 -0600

    Add 2022 season code, fitting it to this year
    Claw and Slide haven't been implemented yet, but purpose is to add things that will be done later

---
+++ TeamCode/src/main/java/org.firstinspires.ftc.teamcode/Drive/TankDrive2p.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.Drive;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.Core.SystemsManagerDrive;
+
+
+/**
+ * XDrive 2 Player
+ * Designed for driving to be on controller 1, upper systems on controller 2.
+ * X drive. Left stick controls coordinate movement, right stick controls rotation.
+ * See ClawCore.java and SlideCore.java for information regarding the claw and slide.
+ */
+@TeleOp(name = "XDrive2P", group = "x")
+public class TankDrive2p extends SystemsManagerDrive {
+    @Override
+    public void loop() {
+        telemetry.addData("STATUS: ", "Running");
+        updateMotor(1);
+    }
+}
