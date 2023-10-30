b4060862609fc4d46973336ca9d631fae9e4db7c
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2023-10-30 16:06:36 -0600

    Create SystemsManagerDrive.java
    Currently only intializes DrivetrainCore

---
+++ TeamCode/src/main/java/org.firstinspires.ftc.teamcode/Core/SystemsManagerDrive.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.Core;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public abstract class SystemsManagerDrive extends OpMode {
+    DrivetrainCore drivetrainCore;
+
+    @Override
+    public void init() {
+        drivetrainCore.hardwareMap(hardwareMap);
+    }
+}
