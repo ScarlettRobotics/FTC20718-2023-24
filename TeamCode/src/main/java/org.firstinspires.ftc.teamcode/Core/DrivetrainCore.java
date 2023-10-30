31ea2bb6442bb2f8223fcc66085908ef1f84fffb
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2023-10-30 16:27:40 -0600

    Make DrivetrainCore.java more readable via comments
    comment on this commit if ugly

--- TeamCode/src/main/java/org.firstinspires.ftc.teamcode/Core/DrivetrainCore.java
+++ TeamCode/src/main/java/org.firstinspires.ftc.teamcode/Core/DrivetrainCore.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode.Core;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 import java.util.ArrayList;
 
@@ -10,17 +9,22 @@ import java.util.ArrayList;
  * This drivetrain uses an X-drive for the robot, using an ArrayList to init DcMotors.
  * Requires an expansion hub, as all motors will be taken on the control hub. */
 public class DrivetrainCore {
-    // Initialize motor variables
-    private ArrayList<DcMotor> motors;
+    /* Initialization */
+    // Drive motors in ArrayList[0-3]
+    // Starts at front right, then goes counter-clockwise from top view.
+    // This is done to fit the same style as a unit circle.
+    private ArrayList<DcMotor> driveMotors;
 
     // Map motor variables to driver hub
     public DrivetrainCore(HardwareMap hardwareMap) {
-        motors = new ArrayList<DcMotor>();
+        driveMotors = new ArrayList<DcMotor>();
+        // hardwareMap
         for (int i=0; i<4; i++) {
-            motors.add(hardwareMap.get(DcMotor.class, "driveMotor"+i));
+            driveMotors.add(hardwareMap.get(DcMotor.class, "driveMotor"+i));
         }
 
-        for (DcMotor i : motors) {
+        // runMode
+        for (DcMotor i : driveMotors) {
             i.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
     }
