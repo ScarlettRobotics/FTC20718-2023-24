3ff189920d3d9d083059e930934140e48f546e19
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2023-11-17 12:35:33 -0700

    Add updateClaw() to TeleOp methods

--- TeamCode/src/main/java/org.firstinspires.ftc.teamcode/Drive/XDrive2p.java
+++ TeamCode/src/main/java/org.firstinspires.ftc.teamcode/Drive/XDrive2p.java
@@ -16,6 +16,7 @@ public class XDrive2p extends SystemsManager {
     public void loop() {
         telemetry.addData("STATUS: ", "Running");
         updateDrivetrain(1);
+        updateClaw(2);
         telemetry(telemetry);
     }
 }
