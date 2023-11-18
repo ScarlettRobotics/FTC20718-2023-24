151b2488d7c4214e861155df1ffef0b508fb794b
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2023-11-17 18:15:56 -0700

    Create DroneEncoderFinder.java
    Serves to find the position that the drone launcher opens at.
    Uses gamepad1 left joystick to set servo position.
    Telemetry can be viewed to see good values.

---
+++ TeamCode/src/main/java/org.firstinspires.ftc.teamcode/Concepts/DroneEncoderFinder.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.Concepts;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.Core.ClawCore;
+import org.firstinspires.ftc.teamcode.Core.DroneLauncherCore;
+
+/** DroneEncoderFinder
+ * Serves to find the position that the drone launcher opens at.
+ * Uses gamepad1 left joystick to set servo position.
+ * Telemetry can be viewed to see good values. */
+@TeleOp(name = "DroneEncoderFinder", group = "concepts")
+public class DroneEncoderFinder extends OpMode {
+    public DroneLauncherCore droneLauncherCore;
+    double left, right;
+
+    @Override
+    public void init() {
+        droneLauncherCore = new DroneLauncherCore(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        double left;
+        left = gamepad1.left_stick_y;
+        droneLauncherCore.moveByPosition(left/1000);
+        droneLauncherCore.telemetry(telemetry);
+    }
+}
