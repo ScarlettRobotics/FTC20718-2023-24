8016a6befbc0798904f25082b2638cf2c0c29da9
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2023-12-01 16:54:08 -0700

    Create XDrive1p.java

---
+++ TeamCode/src/main/java/org.firstinspires.ftc.teamcode/Drive/XDrive1p.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.Drive;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.Core.SystemsManager;
+
+
+/**
+ * XDrive 1 Player
+ * Designed for driving and upper systems to be on controller 1.
+ * X drive. Left stick controls coordinate movement, right stick controls rotation.
+ * See ClawCore.java and SlideCore.java for information regarding the claw and slide.
+ */
+@TeleOp(name = "XDrive2P", group = "x")
+public class XDrive1p extends SystemsManager {
+    @Override
+    public void loop() {
+        telemetry.addData("STATUS: ", "Running");
+        updateDrivetrain(1);
+        updateArm(1);
+        updateClaw(1);
+        checkForDroneLaunch(1);
+        telemetry(telemetry);
+    }
+}
