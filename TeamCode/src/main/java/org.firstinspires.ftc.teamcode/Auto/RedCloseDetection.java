2ef65c419e5316bcf46a10f213087130ff96b7ab
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2023-12-11 15:28:18 -0700

    RedCloseDetection: Boilerplate
    Did core init, telemetry, event checks and instructions
    
    Still need to do default and conditional movements, prop detection

---
+++ TeamCode/src/main/java/org.firstinspires.ftc.teamcode/Auto/RedCloseDetection.java
@@ -0,0 +1,149 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.checkerframework.checker.units.qual.C;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.Core.*;
+
+/** Used if on the close side of movement. Place where wheels touch right teeth.
+ * Places yellow pixel based on prop position, then places purple pixel on backdrop based on prop position. */
+@Autonomous(name = "RedCloseDetection", group = "red")
+public class RedCloseDetection extends LinearOpMode {
+    // FTC Dashboard
+    private FtcDashboard dashboard;
+    private Telemetry dashboardTelemetry;
+    // Timing related
+    private ElapsedTime timer;
+    private EventManager eventManager;
+    // Core classes
+    DrivetrainCore drivetrainCore;
+    ArmCore armCore;
+    ClawCore clawCore;
+    TensorFlowCore tensorFlowCore;
+
+    @Override
+    public void runOpMode() {
+        initialize();
+
+        waitForStart();
+        timer.reset();
+
+        while (opModeIsActive()) {
+            updateAuto();
+
+            if (eventManager.eventOccurred(timer.time(), 0)) {
+                /* TODO INSTRUCTION */
+            } // look for prop
+            if (eventManager.eventOccurred(timer.time(), 1)) {
+                /* TODO INSTRUCTION */
+            } // find prop, move towards position based on prop, move arm to safe
+            if (eventManager.eventOccurred(timer.time(), 2)) {
+                /* TODO INSTRUCTION */
+            } // rotate based on prop, move arm to ground
+
+            if (eventManager.eventOccurred(timer.time(), 3)) {
+                /* TODO INSTRUCTION */
+            } // open claw
+            if (eventManager.eventOccurred(timer.time(), 4)) {
+                /* TODO INSTRUCTION */
+            } // slightly move arm up
+            if (eventManager.eventOccurred(timer.time(), 5)) {
+                /* TODO INSTRUCTION */
+            } // close claw
+
+            if (eventManager.eventOccurred(timer.time(), 6)) {
+                /* TODO INSTRUCTION */
+            } // rotate back, move claw up
+            if (eventManager.eventOccurred(timer.time(), 7)) {
+                /* TODO INSTRUCTION */
+            } // strafe based on prop
+
+            if (eventManager.eventOccurred(timer.time(), 8)) {
+                /* TODO INSTRUCTION */
+            } // move forward to backdrop
+            if (eventManager.eventOccurred(timer.time(), 9)) {
+                /* TODO INSTRUCTION */
+            } // drop pixel on backdrop
+            if (eventManager.eventOccurred(timer.time(), 10)) {
+                /* TODO INSTRUCTION */
+            } // move backwards, reset arm
+
+            if (eventManager.eventOccurred(timer.time(), 11)) {
+                /* TODO INSTRUCTION */
+            } // strafe to left square based on prop
+            if (eventManager.eventOccurred(timer.time(), 12)) {
+                /* TODO INSTRUCTION */
+            } // move forwards into park
+
+            addTelemetry(telemetry);
+        }
+    }
+
+    private void initialize() {
+        // Init dashboard
+        dashboard = FtcDashboard.getInstance();
+        dashboardTelemetry = dashboard.getTelemetry();
+        // Init timing related
+        timer = new ElapsedTime(ElapsedTime.Resolution.SECONDS);
+        eventManager = new EventManager();
+        // Init timings
+        eventManager.addEvent(2);
+        //TODO eventManager.addEvent(xx); // look for prop
+        //TODO eventManager.addEvent(xx); // find prop, move towards position based on prop, move arm to safe
+        //TODO eventManager.addEvent(xx); // rotate based on prop, move arm to ground
+
+        //TODO eventManager.addEvent(xx); // open claw
+        //TODO eventManager.addEvent(xx); // slightly move arm up
+        //TODO eventManager.addEvent(xx); // close claw
+
+        //TODO eventManager.addEvent(xx); // rotate back, move claw up
+        //TODO eventManager.addEvent(xx); // strafe based on prop
+
+        //TODO eventManager.addEvent(xx); // move forward to backdrop
+        //TODO eventManager.addEvent(xx); // drop pixel on backdrop
+        //TODO eventManager.addEvent(xx); // move backwards, reset arm
+
+        //TODO eventManager.addEvent(xx); // strafe to left square based on prop
+        //TODO eventManager.addEvent(xx); // move forwards into park
+        // Init core classes
+        drivetrainCore = new DrivetrainCore(hardwareMap);
+        armCore = new ArmCore(hardwareMap);
+        clawCore = new ClawCore(hardwareMap);
+        tensorFlowCore = new TensorFlowCore(hardwareMap);
+        // Init telemetry
+        telemetry.addData("STATUS", "Initialized");
+        telemetry.update();
+        dashboardTelemetry.addData("STATUS", "Initialized");
+        dashboardTelemetry.update();
+        // Close claw to grip pixels
+        clawCore.close();
+    }
+
+    private void updateAuto() {
+        //drivetrainCore.updateAuto(); TODO
+        //armCore.updateAuto(); TODO
+    }
+
+    private void addTelemetry(Telemetry telemetry) {
+        // Telemetry
+        telemetry.addData("timer", timer.time());
+        eventManager.telemetry(telemetry); //reuse ".telemetry(telemetry)"
+        drivetrainCore.telemetry(telemetry);
+        armCore.telemetry(telemetry);
+        clawCore.telemetry(telemetry);
+        tensorFlowCore.telemetry(telemetry);
+        telemetry.update();
+        // FTC Dashboard
+        dashboardTelemetry.addData("timer", timer.time());
+        eventManager.telemetry(dashboardTelemetry); //reuse ".telemetry(dashboardTelemetry)"
+        drivetrainCore.telemetry(dashboardTelemetry);
+        armCore.telemetry(dashboardTelemetry);
+        clawCore.telemetry(dashboardTelemetry);
+        tensorFlowCore.telemetry(dashboardTelemetry);
+        dashboardTelemetry.update();
+    }
+}
