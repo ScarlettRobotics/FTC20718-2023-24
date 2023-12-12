13db1f626bdacd82d6edf918a0cadb7d22e611ff
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2023-12-11 17:35:17 -0700

    Replace events with incorrect instructions
    Moved parts are right, but encoder numbers aren't

--- TeamCode/src/main/java/org.firstinspires.ftc.teamcode/Auto/RedCloseDetection.java
+++ TeamCode/src/main/java/org.firstinspires.ftc.teamcode/Auto/RedCloseDetection.java
@@ -24,6 +24,8 @@ public class RedCloseDetection extends LinearOpMode {
     ArmCore armCore;
     ClawCore clawCore;
     TensorFlowCore tensorFlowCore;
+    // Other
+    int propLocation; // 0-1-2 is left-middle-right
 
     @Override
     public void runOpMode() {
@@ -35,48 +37,85 @@ public class RedCloseDetection extends LinearOpMode {
         while (opModeIsActive()) {
             updateAuto();
 
+            if (timer.time() < 1.5) {
+                if (!tensorFlowCore.recognizing()) {
+                    continue;
+                } // not recognizing any cubes
+                if (tensorFlowCore.getX() > (double) 640 /2) propLocation = 1; // to left of camera
+                else propLocation = 2; // to right of camera
+            }
             if (eventManager.eventOccurred(timer.time(), 0)) {
-                /* TODO INSTRUCTION */
-            } // look for prop
-            if (eventManager.eventOccurred(timer.time(), 1)) {
-                /* TODO INSTRUCTION */
+                if (!tensorFlowCore.recognizing()) { // not recognizing any cubes
+                    propLocation = 0;
+                    drivetrainCore.forwardByEncoder(100);
+                } else if (tensorFlowCore.getX() > (double) 640 /2) { // to left of camera
+                    propLocation = 1;
+                    drivetrainCore.forwardByEncoder(300);
+                } else { // to right of camera
+                    propLocation = 2;
+                    drivetrainCore.forwardByEncoder(200);
+                }
+                armCore.setTargetPosition(-1800);
             } // find prop, move towards position based on prop, move arm to safe
-            if (eventManager.eventOccurred(timer.time(), 2)) {
-                /* TODO INSTRUCTION */
+            if (eventManager.eventOccurred(timer.time(), 1)) {
+                if (propLocation ==  0) {
+                    drivetrainCore.rotateByEncoder(-100);
+                } else if (propLocation == 2) {
+                    drivetrainCore.rotateByEncoder(100);
+                }
+                armCore.setTargetPosition(-2300);
             } // rotate based on prop, move arm to ground
 
-            if (eventManager.eventOccurred(timer.time(), 3)) {
-                /* TODO INSTRUCTION */
+            if (eventManager.eventOccurred(timer.time(), 2)) {
+                clawCore.open();
             } // open claw
-            if (eventManager.eventOccurred(timer.time(), 4)) {
-                /* TODO INSTRUCTION */
+            if (eventManager.eventOccurred(timer.time(), 3)) {
+                armCore.setTargetPosition(-2250);
             } // slightly move arm up
-            if (eventManager.eventOccurred(timer.time(), 5)) {
-                /* TODO INSTRUCTION */
+            if (eventManager.eventOccurred(timer.time(), 4)) {
+                clawCore.close();
             } // close claw
 
-            if (eventManager.eventOccurred(timer.time(), 6)) {
-                /* TODO INSTRUCTION */
+            if (eventManager.eventOccurred(timer.time(), 5)) {
+                if (propLocation == 0) {
+                    drivetrainCore.rotateByEncoder(850);
+                } else if (propLocation == 2) {
+                    drivetrainCore.rotateByEncoder(650);
+                }
+                armCore.setTargetPosition(-2000);
             } // rotate back, move claw up
-            if (eventManager.eventOccurred(timer.time(), 7)) {
-                /* TODO INSTRUCTION */
+            if (eventManager.eventOccurred(timer.time(), 6)) {
+                if (propLocation == 0) {
+                    drivetrainCore.strafeByEncoder(-150);
+                } else if (propLocation == 1) {
+                    drivetrainCore.strafeByEncoder(-100);
+                } else {
+                    drivetrainCore.strafeByEncoder(-50);
+                }
             } // strafe based on prop
 
-            if (eventManager.eventOccurred(timer.time(), 8)) {
-                /* TODO INSTRUCTION */
+            if (eventManager.eventOccurred(timer.time(), 7)) {
+                drivetrainCore.forwardByEncoder(1000);
             } // move forward to backdrop
-            if (eventManager.eventOccurred(timer.time(), 9)) {
-                /* TODO INSTRUCTION */
+            if (eventManager.eventOccurred(timer.time(), 8)) {
+                clawCore.open();
             } // drop pixel on backdrop
-            if (eventManager.eventOccurred(timer.time(), 10)) {
-                /* TODO INSTRUCTION */
+            if (eventManager.eventOccurred(timer.time(), 9)) {
+                drivetrainCore.forwardByEncoder(-300);
+                armCore.setTargetPosition(-500);
             } // move backwards, reset arm
 
-            if (eventManager.eventOccurred(timer.time(), 11)) {
-                /* TODO INSTRUCTION */
+            if (eventManager.eventOccurred(timer.time(), 10)) {
+                if (propLocation == 0) {
+                    drivetrainCore.strafeByEncoder(-50);
+                } else if (propLocation == 1) {
+                    drivetrainCore.strafeByEncoder(-100);
+                } else {
+                    drivetrainCore.strafeByEncoder(-150);
+                }
             } // strafe to left square based on prop
-            if (eventManager.eventOccurred(timer.time(), 12)) {
-                /* TODO INSTRUCTION */
+            if (eventManager.eventOccurred(timer.time(), 11)) {
+                drivetrainCore.forwardByEncoder(200);
             } // move forwards into park
 
             addTelemetry(telemetry);
@@ -91,23 +130,22 @@ public class RedCloseDetection extends LinearOpMode {
         timer = new ElapsedTime(ElapsedTime.Resolution.SECONDS);
         eventManager = new EventManager();
         // Init timings
-        //TODO eventManager.addEvent(1); // look for prop
-        //TODO eventManager.addEvent(1.5); // find prop, move towards position based on prop, move arm to safe
-        //TODO eventManager.addEvent(2.5); // rotate based on prop, move arm to ground
+        eventManager.addEvent(1); // find prop, move towards position based on prop, move arm to safe
+        eventManager.addEvent(2); // rotate based on prop, move arm to ground
 
-        //TODO eventManager.addEvent(3.5); // open claw
-        //TODO eventManager.addEvent(3.8); // slightly move arm up
-        //TODO eventManager.addEvent(4.1); // close claw
+        eventManager.addEvent(3); // open claw
+        eventManager.addEvent(3.3); // slightly move arm up
+        eventManager.addEvent(3.6); // close claw
 
-        //TODO eventManager.addEvent(4.4); // rotate back, move claw up
-        //TODO eventManager.addEvent(5.5); // strafe based on prop
+        eventManager.addEvent(3.9); // rotate back, move claw up
+        eventManager.addEvent(5); // strafe based on prop
 
-        //TODO eventManager.addEvent(6.5); // move forward to backdrop
-        //TODO eventManager.addEvent(8); // drop pixel on backdrop
-        //TODO eventManager.addEvent(8.5); // move backwards, reset arm
+        eventManager.addEvent(6); // move forward to backdrop
+        eventManager.addEvent(7.5); // drop pixel on backdrop
+        eventManager.addEvent(8); // move backwards, reset arm
 
-        //TODO eventManager.addEvent(9); // strafe to left square based on prop
-        //TODO eventManager.addEvent(10.5); // move forwards into park
+        eventManager.addEvent(8.5); // strafe to left square based on prop
+        eventManager.addEvent(10); // move forwards into park
         // Init core classes
         drivetrainCore = new DrivetrainCore(hardwareMap);
         armCore = new ArmCore(hardwareMap);
