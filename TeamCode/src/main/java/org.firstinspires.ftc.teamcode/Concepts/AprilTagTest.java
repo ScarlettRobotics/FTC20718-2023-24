c9fe6538fb256765e7fe98f6bef77cf03dc1b3de
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2024-01-23 12:21:44 -0700

    Change AprilTagTest to base off AprilTagCore
    OOP go brrrr

---
+++ TeamCode/src/main/java/org.firstinspires.ftc.teamcode/Concepts/AprilTagTest.java
@@ -0,0 +1,68 @@
+
+package org.firstinspires.ftc.teamcode.Concepts;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.Core.AprilTagCore;
+
+/**
+ * This OpMode illustrates the basics of AprilTag recognition and pose estimation,
+ * including Java Builder structures for specifying Vision parameters.
+ *
+ * For an introduction to AprilTags, see the FTC-DOCS link below:
+ * https://ftc-docs.firstinspires.org/en/latest/apriltag/vision_portal/apriltag_intro/apriltag-intro.html
+ *
+ * In this sample, any visible tag ID will be detected and displayed, but only tags that are included in the default
+ * "TagLibrary" will have their position and orientation information displayed.  This default TagLibrary contains
+ * the current Season's AprilTags and a small set of "test Tags" in the high number range.
+ *
+ * When an AprilTag in the TagLibrary is detected, the SDK provides location and orientation of the tag, relative to the camera.
+ * This information is provided in the "ftcPose" member of the returned "detection", and is explained in the ftc-docs page linked below.
+ * https://ftc-docs.firstinspires.org/apriltag-detection-values
+ *
+ * To experiment with using AprilTags to navigate, try out these two driving samples:
+ * RobotAutoDriveToAprilTagOmni and RobotAutoDriveToAprilTagTank
+ *
+ * There are many "default" VisionPortal and AprilTag configuration parameters that may be overridden if desired.
+ * These default parameters are shown as comments in the code below.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ */
+@TeleOp(name = "Concept: AprilTag", group = "concept-apriltag")
+public class AprilTagTest extends LinearOpMode {
+    private AprilTagCore aprilTagCore;
+
+    @Override
+    public void runOpMode() {
+        aprilTagCore = new AprilTagCore(hardwareMap, 2);
+
+        // Wait for the DS start button to be touched.
+        telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
+        telemetry.addData(">", "Touch Play to start OpMode");
+        telemetry.update();
+        waitForStart();
+
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
+
+                aprilTagCore.telemetry(telemetry);
+                telemetry.update();
+
+                // Save CPU resources; can resume streaming when needed.
+                if (gamepad1.dpad_down) {
+                    aprilTagCore.stopStreaming();
+                } else if (gamepad1.dpad_up) {
+                    aprilTagCore.resumeStreaming();
+                }
+
+                // Share the CPU.
+                sleep(20);
+            }
+        }
+
+        // Save more CPU resources when camera is no longer needed.
+        aprilTagCore.closeVisionPortal();
+
+    }   // end method runOpMode()
+
+}   // end class
