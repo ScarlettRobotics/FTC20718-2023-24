82dcf7666dba16cb766bacdffd608c9502ef6dde
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2024-02-23 09:46:10 -0700

    Follower PID Tuning
    https://learnroadrunner.com/follower-pid-tuning.html

--- TeamCode/src/main/java/org.firstinspires.ftc.teamcode/RRdrive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org.firstinspires.ftc.teamcode/RRdrive/SampleMecanumDrive.java
@@ -49,8 +49,8 @@ import static org.firstinspires.ftc.teamcode.RRdrive.DriveConstants.kV;
  */
 @Config
 public class SampleMecanumDrive extends MecanumDrive {
-    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(0, 0, 0);
-    public static PIDCoefficients HEADING_PID = new PIDCoefficients(0, 0, 0);
+    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(9.2, 0, 0);
+    public static PIDCoefficients HEADING_PID = new PIDCoefficients(8, 0, 0);
 
     public static double LATERAL_MULTIPLIER = 1.0060581474;
 
