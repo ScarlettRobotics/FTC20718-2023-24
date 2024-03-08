94e7deed7ae8d7f8dff18790ad7393b5baece885
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2024-03-08 16:27:32 -0700

    Delete unneeded imports

--- TeamCode/src/main/java/org.firstinspires.ftc.teamcode/OpModeAuto/RedClose40.java
+++ TeamCode/src/main/java/org.firstinspires.ftc.teamcode/OpModeAuto/RedClose40.java
@@ -1,20 +1,6 @@
 package org.firstinspires.ftc.teamcode.OpModeAuto;
 
-import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
-import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.teamcode.AutoCore.AprilTagCore;
-import org.firstinspires.ftc.teamcode.AutoCore.TensorFlowCore;
-import org.firstinspires.ftc.teamcode.AutoCore.VisionPortalCore;
-import org.firstinspires.ftc.teamcode.Core.ClawCore;
-import org.firstinspires.ftc.teamcode.RRdrive.SampleMecanumDrive;
-
-import java.util.ArrayList;
 
 /** Uses RoadRunner to score 40 autonomous points (20 from purple, 20 from yellow).
  * This class only works on RedClose position */
--- TeamCode/src/main/java/org.firstinspires.ftc.teamcode/OpModeAuto/RoadRunnerStarter.java
+++ TeamCode/src/main/java/org.firstinspires.ftc.teamcode/OpModeAuto/RoadRunnerStarter.java
@@ -1,11 +1,6 @@
 package org.firstinspires.ftc.teamcode.OpModeAuto;
 
 import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
-import com.acmerobotics.roadrunner.trajectory.Trajectory;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -15,12 +10,9 @@ import org.firstinspires.ftc.teamcode.AutoCore.VisionPortalCore;
 import org.firstinspires.ftc.teamcode.Core.ClawCore;
 import org.firstinspires.ftc.teamcode.RRdrive.SampleMecanumDrive;
 
-import java.util.ArrayList;
-
 /** Abstract class containing all mechanical initializations with no trajectories.
  * Starting auto for all RoadRunner autonomous programs.
  * Done so that a mechanical change won't have to be done on all classes. */
-@Disabled
 public abstract class RoadRunnerStarter extends LinearOpMode {
     // FTC Dashboard
     private FtcDashboard dashboard;
