3bd74b7dd85f8c8456d6c2dff94281e8224c0d9a
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2024-03-13 09:21:27 -0600

    ArmCore: Start work on feedforward as gravity compensation
    https://www.ctrlaltftc.com/feedforward-control

--- TeamCode/src/main/java/org.firstinspires.ftc.teamcode/Core/ArmCore.java
+++ TeamCode/src/main/java/org.firstinspires.ftc.teamcode/Core/ArmCore.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode.Core;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.teamcode.AutoCore.PIDController;
 import org.firstinspires.ftc.teamcode.AutoCore.PIDControllerSimple;
 
 /** Operates the arm of the robot.
@@ -15,7 +14,9 @@ public class ArmCore {
     private PIDControllerSimple pid;
     final private double Kcos = 0;
     final private int startAngle = 0; //TODO TUNE
-    private DcMotor armMotor;
+    final private DcMotor armMotor;
+
+    final private double ENCODER_TO_ANGLE = 1;
 
     public ArmCore(HardwareMap hardwareMap) {
         pid = new PIDControllerSimple("armMotor",
@@ -23,21 +24,21 @@ public class ArmCore {
         armMotor = hardwareMap.get(DcMotor.class, "armMotor");
     }
 
-    /** Sets a new target position for the motor. */
+    /** Sets a new target position for the motor. TODO WILL BE MADE REDUNDANT */
     public void setTargetPosition(int encoder) {
         pid.setTargetPosition(encoder);
     }
 
     /** Sets a new target angle for the motor.
-     * 0 degrees is when the arm is perfectly vertical.
+     * 0 degrees is when the arm is first parallel with the ground.
      * Positive angle movement is out from starting position.
      * Use degrees. */
     public void setTargetAngle(double targetAngle) {
-        int encoder = (int) ((targetAngle - startAngle) * 10); // TODO CHANGE THIS COEFFICIENT
+        int encoder = (int) ((targetAngle - startAngle) / ENCODER_TO_ANGLE); // TODO CHANGE THIS COEFFICIENT
         pid.setTargetPosition(encoder);
     }
 
-    /** Changes the encoder position by the inputted amount. */
+    /** Changes the encoder position by the inputted amount. TODO WILL BE MADE REDUNDANT */
     public void moveByEncoder(int encoder) {
         pid.moveByEncoder(encoder);
     }
@@ -45,12 +46,17 @@ public class ArmCore {
     /** Updates the PIDController to move towards the provided goal position. */
     public void updateAuto() {
         pid.update(armMotor.getCurrentPosition());
+        double gravityCompensation = Kcos * Math.cos(encoderToAngle((int) pid.getTargetPosition()));
         armMotor.setPower(pid.getPower());
     }
 
     /** Moves the arm motor, ignoring autonomous in the process */
     protected void setPower(double power){
         armMotor.setPower(power);
+    }
+
+    private double encoderToAngle(int encoder) {
+        return encoder * ENCODER_TO_ANGLE;
     }
 
     /** Telemetry */
