6543718b1cdd101bad9e65ee3831ec690efd75a4
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2024-03-14 17:21:20 -0600

    Try to do AprilTagDetection (big fail)

--- TeamCode/src/main/java/org.firstinspires.ftc.teamcode/Concepts/AprilTagTest.java
+++ TeamCode/src/main/java/org.firstinspires.ftc.teamcode/Concepts/AprilTagTest.java
@@ -39,13 +39,12 @@ import java.util.ArrayList;
 public class AprilTagTest extends LinearOpMode {
     private VisionPortalCore visionPortalCore;
     private AprilTagCore aprilTagCore;
-    private double range, xDist, yDist, heading;
-    private Pose2d aprilTagPose = new Pose2d();
     final ArrayList<Vector2d> aprilTagCoords = new ArrayList<>();
-    Vector2d detectedTagCoords = new Vector2d();
-    // offset is 8.125" (x) by 1.5" (y)
-    final double centerOffsetMagnitude = 8.262; // Distance from camera to robot's center (pythagorean theorem)
-    final double centerOffsetHeading = 10.46; // Heading that centerOffsetMagnitude faces when the robot's heading is 0 (TOA)
+    private double range;
+    private Vector2d detectedTagCoords = new Vector2d();
+    private Vector2d robotDistance = new Vector2d();
+    private double heading;
+    private Pose2d aprilTagPose = new Pose2d();
 
     @Override
     public void runOpMode() {
@@ -97,46 +96,62 @@ public class AprilTagTest extends LinearOpMode {
 
     }   // end method runOpMode()
 
-    /** When an AprilTag is detected, the robot can very precisely tell where it is by using ftcPose data.
-     * Make sure that the robot is 100% detecting an AprilTag or nothing will happen */
+    /** Removes error from previous movement by setting current pose based on AprilTag detection.
+     * When an AprilTag is detected, the robot can very precisely tell where it is by using ftcPose data.
+     * Make sure that the robot is 100% detecting an AprilTag or nothing will happen. */
     private void detectPositionFromAprilTag() {
+        // Distance from robot's center to camera; signs accurate
+        Vector2d centerOffsetDistance = new Vector2d(8.125, 1.5);
+
         if (aprilTagCore.getDetections().isEmpty()) return;
-        // Remove error from previous movement by setting current pose based on AprilTag detection
         AprilTagDetection detectedTag = aprilTagCore.getDetections().get(0);
         // Camera doesn't point parallel with ground so mathematical adjustments have to be made
         range = detectedTag.ftcPose.range * Math.cos(Math.toRadians(23));
 
-        double theta = Math.toRadians(detectedTag.ftcPose.yaw + detectedTag.ftcPose.bearing); // see math notes
-        // Robot X-coordinate relative to AprilTag (in field orientation)
-        xDist = range * Math.cos(theta);
-        // Robot Y-coordinate relative to AprilTag (in field orientation)
-        yDist = range * Math.sin(theta);
-        // Robot heading relative to field; RoadRunner specific
-        heading = -detectedTag.ftcPose.yaw;
+        // see math notes
+        double ACD = Math.toRadians(90 - detectedTag.ftcPose.bearing);
+        double DCO = Math.toRadians(90 - centerOffsetDistance.angle());
+        double ACO = DCO + ACD;
+        double AC = range;
+        double CO = centerOffsetDistance.norm();
+        double AO = Math.sqrt(AC*AC + CO*CO - 2*AC*CO*Math.cos(ACO)); // cosine law
+        double AOC = Math.asin((AC * Math.sin(ACO)) / AO); // sine law
+        double COM = centerOffsetDistance.angle();
+        // Robot center heading relative to field; RoadRunner specific
+        heading = Math.toDegrees(AOC - COM);
+
+        // see math notes
+        double CAO = Math.PI - AOC - ACO;
+        double CAF = ACD;
+        double BAF = detectedTag.ftcPose.yaw;
+        double BAO = CAO + CAF + BAF;
+        // How far the robot's center is from the AprilTag, in field orientation (x, y)
+        robotDistance = new Vector2d(
+                AO * Math.sin(BAO),
+                AO * Math.cos(BAO)
+        );
 
         detectedTagCoords = aprilTagCoords.get(detectedTag.id-1);
         aprilTagPose = new Pose2d(
                 new Vector2d(
-                        (detectedTagCoords.getX() - xDist) - // camera's x coordinate
-                                (Math.cos(Math.toRadians(heading + centerOffsetHeading)) * centerOffsetMagnitude), // move coordinate to robot's center
-                        (detectedTagCoords.getY() - yDist) - // camera's y coordinate
-                                (Math.sin(Math.toRadians(heading + centerOffsetHeading)) * centerOffsetMagnitude)), // move coordinate to robot's center
+                        detectedTagCoords.getX() + robotDistance.getX(),
+                        detectedTagCoords.getY() + robotDistance.getY()),
                 heading);
     }
 
     private void telemetry(Telemetry telemetry) {
         telemetry.addData("range", range);
-        telemetry.addData("xDist", xDist);
-        telemetry.addData("yDist", yDist);
+        telemetry.addData("\ndetectedTagCoords", "%.1f, %.1f",
+                detectedTagCoords.getX(),
+                detectedTagCoords.getY());
+        telemetry.addData("\nrobotDistance", "%.1f, %.1f",
+                robotDistance.getX(),
+                robotDistance.getY());
         telemetry.addData("heading", heading);
         telemetry.addData("\nPose data", " %.2f %.2f %.0f",
                 aprilTagPose.getX(),
                 aprilTagPose.getY(),
                 aprilTagPose.getHeading());
-        telemetry.addData("\nCamera's X coordinate", (detectedTagCoords.getX() - xDist));
-        telemetry.addData("X offset to robot's center", (Math.cos(Math.toRadians(heading + centerOffsetHeading)) * centerOffsetMagnitude));
-        telemetry.addData("Camera's Y coordinate", (detectedTagCoords.getY() - yDist));
-        telemetry.addData("Y offset to robot's center", (Math.sin(Math.toRadians(heading + centerOffsetHeading)) * centerOffsetMagnitude));
         
         aprilTagCore.telemetry(telemetry);
         telemetry.update();
